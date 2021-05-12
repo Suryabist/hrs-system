c6fc354acecc679d645e8c4f9eece6f4b83d5e9e
surya <sbistaiw@gmail.com>
2021-05-12 17:03:39 +0545

    Added regex search

--- hospital/core/api/v1/views/hospital.py
+++ hospital/core/api/v1/views/hospital.py
@@ -14,7 +14,7 @@ class hospitalViewSet(viewsets.ModelViewSet):
     serializer_class = HospitalSerializer
     queryset = hospitals.objects.all()
     filter_backends = (filters.SearchFilter,)
-    search_fields = ['name']
+    search_fields = ['$name']
 
 
 
