f492ceac0b10ccfcf0d05e4fb822e98b82a34c9b
surya <sbistaiw@gmail.com>
2021-05-15 15:14:39 +0545

    Updated the images url

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,8 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.views.generic import RedirectView
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -32,3 +34,4 @@ if settings.DEBUG:
                     path('', RedirectView.as_view(url='/api/root/', permanent=False)),
                     re_path(r'^__debug__/', include(debug_toolbar.urls)),
                     ]
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
