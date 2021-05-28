f21c33e638ba955813018fd7e0676d68e8567318
surya <sbistaiw@gmail.com>
2021-05-28 10:06:05 +0545

    Updated the permisison

--- hospital/commons/api/v1/views/contact.py
+++ hospital/commons/api/v1/views/contact.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
 
 from hospital.commons.api.v1.serializers.contact import ContactSerializer
 from hospital.commons.models import contact
@@ -7,4 +8,5 @@ from hospital.commons.models import contact
 class ContactViewSet(viewsets.ModelViewSet):
     queryset = contact.objects.all()
     serializer_class = ContactSerializer
+    permission_classes = [AllowAny]
 
--- hospital/commons/api/v1/views/corona.py
+++ hospital/commons/api/v1/views/corona.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
 
 from hospital.commons.api.v1.serializers import corona
 from hospital.commons.api.v1.serializers.corona import CoronaSerializers
@@ -8,4 +9,5 @@ from hospital.commons.models import Corona
 class coronaViewSet(viewsets.ModelViewSet):
     queryset = Corona.objects.all()
     serializer_class = CoronaSerializers
+    permission_classes = [AllowAny]
 
