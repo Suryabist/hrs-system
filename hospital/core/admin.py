a1719936793aba4f67b102ab0dbfbe55b7f850f2
surya <sbistaiw@gmail.com>
2021-06-12 09:53:00 +0545

    Added the export import

--- hospital/core/admin.py
+++ hospital/core/admin.py
@@ -1,10 +1,28 @@
 from django.contrib import admin
 
 # Register your models here.
+from import_export import resources, fields
+from import_export.admin import ImportExportModelAdmin
+from import_export.widgets import ForeignKeyWidget
+
 from hospital.core.models import hospitals, normal_bed, icu_bed, ventilators, OxygenCylinders, discharge, \
     death, focalperson, HduBeds
 
-admin.site.register(hospitals)
+
+class HospitalResource(resources.ModelResource):
+    capacity = fields.Field(column_name='capacity', attribute='icu_bed',
+                              widget=ForeignKeyWidget(icu_bed, 'capacity'))
+
+    class Meta:
+        model = hospitals
+        fields = ('id', 'name', 'address', 'hospital_type', 'phone_no', 'capacity', )
+
+
+class Hospitaladmin(ImportExportModelAdmin):
+    resource_class = HospitalResource
+
+
+admin.site.register(hospitals, Hospitaladmin)
 admin.site.register(normal_bed)
 admin.site.register(icu_bed)
 admin.site.register(HduBeds)
